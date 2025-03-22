960060055314834cd93ee976ab72ac279ea616de
ash66 <aichiko66@163.com>
2025-03-22 09:21:00 +0800

    add AboutUs and About components with basic content

---
+++ src/app/aboutus/page.jsx
@@ -0,0 +1,15 @@
+import About from "@/components/About";
+import React from "react";
+
+export default function AboutUs() {
+
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a team of developers who love to code and create amazing applications.
+      </p>
+      <About />
+    </div>
+  )
+}
